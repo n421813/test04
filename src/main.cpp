aa150d74f0fbb682da403c36e136fcc0e47a51a5
torodaiou <35546015+torodaiou@users.noreply.github.com>
2021-05-28 06:22:02 +0900

    Added buffer overflow case

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,20 @@
 #include <stdlib.h>
 #include "head.h"
 
+void func01(void) {
+    int array[10] = {};
+    array[10] = 5;
+    return;
+}
+
+void func02(void) {
+    return;
+}
+
+void func03(void) {
+    return;
+}
+
 int main(void) {
     int a = 0;
     int b = 0;
