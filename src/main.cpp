cd70d091b0dedd3b017f191b725f88f4c4b622c6
torodaiou <35546015+torodaiou@users.noreply.github.com>
2021-05-28 09:50:12 +0900

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,24 +8,32 @@
 static int array[10];
 
 void func01(void) {
+    // 範囲外アクセス
     array[10] = 5;
     return;
 }
 
 void func02(void) {
-    return;
-}
-
-void func03(void) {
-    return;
-}
-
-int main(void) {
+    // ゼロ割
     int a = 0;
     int b = 0;
     int c = 0;
     c = a/b;
     printf("hello=%d\n", c);
+    return;
+}
+
+void func03(void) {
+    // nullポインタ
+    int *p = nullptr;
+    *p = 123U;
+    return;
+}
+
+int main(void) {
+    func01();
+    func02();
+    func03();
     exit(0);
 }
 
